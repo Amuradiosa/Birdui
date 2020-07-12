12e9bc9da3690e593cdc422522ee3e76da0aa1ac
Ahmad Murad <ahmadmurad@Ahmads-MBP.broadband>
2020-07-12 17:21:11 +0100

    create postViewModal dependency for PostListView, create initial UI with stacks and List for posts list

--- Birdui Materials/final/Birdui/Birdui/PostListView.swift
+++ Birdui Materials/final/Birdui/Birdui/PostListView.swift
@@ -9,15 +9,48 @@
 import SwiftUI
 
 struct PostListView: View {
+  
+  @ObservedObject var postViewModel: PostViewModel
+  @State var createModalIsPresented = false
+//  @State var editModalIsPresented = false
+  
   var body: some View {
+    Form {
+      VStack {
+        HStack {
+          Image("mascot")
+            .resizable()
+            .frame(width: 50, height: 50, alignment: .leading)
+          Text("Home")
+            .bold()
+            .font(.title)
+        }
+        
+        Button("Create New Post") {
+          self.createModalIsPresented = true
+        }
+      }
+      
+      List(postViewModel.posts) { post in
+        Text(post.userName)
+      }
+      
+    }
+    .sheet(isPresented: $createModalIsPresented) {
+      NewPostView(postHandler: self.postViewModel)
+    }
+  
+    
+    
     // TODO: This should look exactly like the Birdie table view,
     // but with only one button.
-    Text("Layout header, new-post button, List of posts")
+    //    Text("Layout header, new-post button, List of posts")
+    
   }
 }
 
 struct PostListView_Previews: PreviewProvider {
   static var previews: some View {
-    PostListView()
+    PostListView(postViewModel: PostViewModel())
   }
 }
